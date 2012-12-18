0643f6432fdab53cba15ca9eb548b333f5e6c9d3
Paul Kulyk <paul.kulyk@gmail.com>
2012-12-18 17:09:13 -0600

    Added end of video termination

--- default.py
+++ default.py
@@ -7,57 +7,49 @@ import os
 start = time.clock()
 
 WINDOW_FULLSCREEN_VIDEO = 12005
+WINDOW_OSD = 12901
 IMAGE_PATH=xbmc.translatePath('special://home/addons/script.helloworld/bike_banner.png')
-HEART_PATH=xbmc.translatePath('special://home/addons/script.helloworld/heart.png')
-WORKOUT=xbmc.translatePath('special://home/addons/script.helloworld/angels')
+
+dialog = xbmcgui.Dialog()
+WORKOUT=xbmc.translatePath(dialog.browse(1,'Workout selection','files','',False,False,'special://home/addons/script.helloworld/workouts/test'))
+workout_file=open(WORKOUT)
 
 window = xbmcgui.Window(WINDOW_FULLSCREEN_VIDEO)
 
 window.show()
 
 # Set up the labels
-#alert_label = xbmcgui.ControlLabel( window.getWidth()/2, window.getHeight()/2, 0, 0, 'Hello, world!', 'WeatherTemp', alignment=0x6 )
 timer_label = xbmcgui.ControlLabel( window.getWidth() - 50, 0, 300, 100, 'Timer', 'font30_title', alignment=0x5)
 details_label = xbmcgui.ControlLabel( 100, 0, window.getWidth() - 300, 100, '', 'font30_title', alignment=0x4)
 image = xbmcgui.ControlImage(0, 0, 1500, 100, IMAGE_PATH)
-#heart = xbmcgui.ControlImage(0, 0, 50, 50, HEART_PATH)
-#heart_rate = xbmcgui.ControlLabel(0,0,100,50,'', 'font30_title', alignment=0x6)
 
 # Add the control to the FullScreenVideo window
-#alert_control = window.addControl(alert_label)
-#image.setAnimations([('visible', 'effect=fade time="500"',), ('hidden', 'effect=fade time="500"',)])
-image_control = window.addControl(image)
-#heart_control = window.addControl(heart)
-#heart_rate_control =window.addControl(heart_rate)
-timer_control = window.addControl(timer_label)
-details_control = window.addControl(details_label)
+#image.setAnimations([('VisibleChange', 'effect=fade time="1000"',)])
+window.addControl(image)
+window.addControl(timer_label)
+window.addControl(details_label)
 
+if xbmc.Player().isPlayingVideo():
+  for line in workout_file:
+    line_split = line.split()
+    interval_time = float(line_split[0])
+    rpe = line_split[1]
+    hr_zone = "(RPE %s)" % (rpe)
+    details = ' '.join(line_split[2:])
+    details_label.setLabel(details + ' ' + hr_zone)
+    start = time.time()
+    now = start
+    while now - start <= interval_time and xbmc.Player().isPlayingVideo(): 
+        time_remaining = interval_time - now + start
+        timer = time.strftime("%H:%M:%S", time.gmtime(time_remaining))
+        timer_label.setLabel(timer)
+        now = time.time()
+        time.sleep(1)
 
-for line in open(WORKOUT):
-  line_split = line.split()
-  interval_time = float(line_split[0])
-  rpe = line_split[1]
-  hr_zone = "(RPE %s)" % (rpe)
-  details = ' '.join(line_split[2:])
-  details_label.setLabel(details + ' ' + hr_zone)
-  #heart_rate.setLabel(hr_zone)
-  start = time.time()
-  now = start
-  while now - start <= interval_time:
-    time_remaining = interval_time - now + start
-    timer = time.strftime("%H:%M:%S", time.gmtime(time_remaining))
-    timer_label.setLabel(timer)
-    now = time.time()
-    time.sleep(1)
+if xbmc.Player().isPlayingVideo():
+  window.removeControl(timer_label)
+  window.removeControl(image)
+  #window.removeControl(progress)
+  window.removeControl(details_label)
 
-    
-
-
-#alert_label.setLabel('')
-
-window.removeControl(timer_label)
-window.removeControl(image)
-window.removeControl(details_label)
-#window.removeControl(heart)
-#window.removeControl(heart_rate)
 
